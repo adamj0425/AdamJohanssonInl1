58457651ec502f70e0e9003972162ddd4e428271
Adam Johansson <adam0425@MacBook-Air-som-tillhor-Adam.local>
2023-11-10 15:58:00 +0100

    improved the script

--- run_game.sh
+++ run_game.sh
@@ -3,12 +3,13 @@ echo Adam Johansson
 mkdir Adam_Johansson_labb
 cp *.java Adam_Johansson_labb
 cd Adam_Johansson_labb
-pwd
+echo Running game from $(pwd)
 echo Compiling...
 javac Guesser.java GuessingGame.java
 echo Running game...
 java GuessingGame
 echo Done
+echo Removing class files...
 rm *.class
 ls
 
