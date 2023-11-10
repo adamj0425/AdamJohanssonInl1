6652d0503b26161d187326f53ead8e6583b4a4c9
Adam Johansson <adam0425@MacBook-Air-som-tillhor-Adam.local>
2023-11-10 15:11:21 +0100

    Updated the getReply method so that it takes input from the user

--- Guesser.java
+++ Guesser.java
@@ -1,4 +1,5 @@
 import java.io.Console;
+import java.util.Scanner;
 
 /**
  * Assignment 1 - Guessing Game
@@ -59,12 +60,23 @@ public class Guesser {
    * the player.
    */
   private String getReply() {
-    String reply = null;
+    Scanner scanner = new Scanner(System.in);
+    String reply = scanner.nextLine();
+    
+  while( !reply.equals ("T") && !reply.equals("F")){
+  System.out.println("please answer T or F"); 
+  reply = scanner.nextLine();
+  }
+return reply;
+
+
+
+
     // Write code here which reads a String from the console.
     // As long as it is not a valid reply (one of "T" and "F")
     // write an error message, and read a new reply.
     // When you have gotten a valid reply, return it.
-    return reply;
+    
   }
 
   private void doGuesses() {
